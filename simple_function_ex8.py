561b3cb896cdc51c90dfd4536e8f810797c89edb
K S Sreenivasa Raghavan <sreenivas.raghavan7@gmail.com>
2022-11-06 23:17:09 +0530

    changed the function0

--- simple_function_ex8.py
+++ simple_function_ex8.py
@@ -7,7 +7,7 @@ print("To Call this function using its name and assign the value of your choice:
        and then call the function inside module like simple_function1.double(5) Or else\
        you can also assign the value obtained from this function to a new variable of\
        your choice for ex: x=simple_function1.double(5), and then print the value of that variable") 
-#x = double(3)
-#print(x)
+x = double(3)
+print(x)
 print("The program ran successfully")
 print("Added now in the techteam12")
